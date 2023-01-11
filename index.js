78f7b7f228a971e642f1f2704f6416403aac5f18
Anthony Simental <anthonysimental12@gmail.com>
2023-01-11 13:00:16 -0800

    complete

--- index.js
+++ index.js
@@ -1,5 +1,77 @@
-// Code your solution here
 // Array Filter Method Lab
-function findMatching () {
-    
+
+// Globals
+const drivers = [
+    'Bobby',
+    'Sammy',
+    'Sally',
+    'Annette',
+    'Sarah',
+    'Bobby'
+];
+
+const drivers2 = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburg'
+    },
+    {
+        name: 'Sammy',
+        hometown: 'New York'
+    },
+    {
+        name: 'Sally',
+        hometown: 'Cleveland'
+    },
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles'
+    },
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    }
+];
+
+// 1.
+function findMatching (array, string) {
+    return array.filter( driver => driver.toLowerCase() === string.toLowerCase() );
 }
+console.log('Find Matching:', findMatching(drivers, 'Bobby'));
+// LOG: Find Matching: (2) ['Bobby', 'Bobby']
+
+
+// 2.
+function fuzzyMatch (array, string) {
+    return array.filter( driver => driver.startsWith(string) );
+}
+console.log('Fuzzy Match:', fuzzyMatch(drivers, 'S'));
+// LOG: Fuzzy Match: (3) ['Sammy', 'Sally', 'Sarah']
+
+
+// 3.
+function matchName (array, string) {
+    return array.filter(function (driver) {
+        return driver.name === string;
+    });
+}
+console.log('Match Name:', matchName(drivers2, 'Bobby'));
+// LOG: { name: 'Bobby', hometown: 'Pittsburg' }
+// LOG: { name: 'Bobby', hometown: 'Tampa Bay' }
+
+
+/*
+const result = drivers.filter(driversName => driversName === string)
+Result will be a new array (generated from filter) that contains the elements meeting the condition in our callback: (driversName === string) 
+*/
+
+/*
+// Deliverables
+You'll be writing three functions:
+1. findMatching- This function takes an array of drivers' names & a string as arguments, & returns the matching list of drivers. The
+   function should be case insensitive.
+2. fuzzyMatch - This function takes an array of drivers' names & a string as arguments for querying the array, & returns all drivers
+   whose names begin with the provided letters.
+3. matchName - This function takes an array of driver objects & a string as arguments. Each driver object has 2 properties: name &
+   hometown. The function should return each element whose name property matches the provided string argument.
+*/
--- notes.js
+++ notes.js
@@ -1,1 +1,116 @@
 // Array Filter Method Lab Notes
+
+// Globals
+const drivers = [
+    'Bobby',
+    'Sammy',
+    'Sally',
+    'Annette',
+    'Sarah',
+    'Bobby'
+];
+
+// #1.
+// Step 1:
+function findMatching (array, string) {
+    return array.filter(function (driver) {
+        return driver === string;
+    })
+}
+// Step 2:
+function findMatching (array, string) {
+    return array.filter(function (driver) {
+        return driver.toLowerCase() === string.toLowerCase();
+    })
+}
+// Step 3:
+function findMatching (array, string) {
+    return array.filter((driver) => {
+        driver.toLowerCase() === string.toLowerCase();
+    })
+}
+// Step 4:
+function findMatching (array, string) {
+    return array.filter( driver => driver.toLowerCase() === string.toLowerCase() );
+}
+console.log(findMatching(drivers, 'Bobby'));
+// LOG: (2) ['Bobby', 'Bobby']
+
+
+// #2.
+// Step 1:
+function fuzzyMatch (array, string) {
+    return array.filter(function (driver) {
+        return driver.startsWith(string);
+    })
+}
+// Step 2:
+function fuzzyMatch (array, string) {
+    return array.filter(function (driver) {
+        return driver.startsWith(string);
+    })
+}
+// Step 3:
+function fuzzyMatch (array, string) {
+    return array.filter((driver) => {
+        driver.startsWith(string);
+    })
+}
+// Step 4:
+function fuzzyMatch (array, string) {
+    return array.filter( driver => driver.startsWith(string) );
+}
+console.log('Fuzzy Match:', fuzzyMatch(drivers, 'S'));
+// LOG: Fuzzy Match: (3) ['Sammy', 'Sally', 'Sarah']
+
+
+// #3.
+// Step 1:
+
+// Step 2:
+
+// Step 3:
+
+// Step 4:
+
+
+/*
+**Anonymous function** inside the .filter ()
+Previous Lesson Example:
+[1, 2, 3, 4, 5].filter(function (num) {
+    return num > 3;
+});
+// => [4, 5]
+Arrow Function:
+[1, 2, 3, 4, 5].filter( num => num > 3; )
+
+
+// Anything that's iterating (map, filter, etc.) will need the place holder for each INDIVIDUAL index for the array 
+*/
+
+
+// Alternate for #2:
+// Step 1:
+function fuzzyMatch (array, str) {
+    return array.filter( function (driver) {
+        return driver.toLowerCase().indexOf(str.toLowerCase()) !== -1;
+    } )
+}
+// Step 2:
+function fuzzyMatch (array, str) {
+    return array.filter( function (driver) {
+        driver.toLowerCase().indexOf(str.toLowerCase()) !== -1;
+    } )
+}
+// Step 3:
+function fuzzyMatch (array, str) {
+    return array.filter( function (driver) {
+        driver.toLowerCase().indexOf(str.toLowerCase()) !== -1;
+    } )
+}
+// Step 4:
+function fuzzyMatch (array, str) {
+    return array.filter( function (driver) {
+        driver.toLowerCase().indexOf(str.toLowerCase()) !== -1;
+    } )
+}
